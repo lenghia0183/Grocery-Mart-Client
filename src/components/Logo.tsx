5b800c32158bc399b67f7d2117c5a6f791e56682
Lenghia0183 <lenghia0183@gmail.com>
2024-11-30 20:28:22 +0700

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,18 +1,26 @@
 import images from "@/asset/images";
 import Image from "./Image";
+import Link from "next/link";
 
 function Logo(): JSX.Element {
   return (
-    <div className="flex items-center gap-4 text-2xl font-semibold text-dark-500">
+    <Link
+      href="/"
+      className="flex items-center gap-4 text-2xl font-semibold text-dark-500 select-none"
+      title="Grocery Mart Logo"
+    >
       <Image
         width={35}
         height={35}
         src={images.logo}
         alt="Grocery Mart Logo - A modern online grocery store"
         title="Grocery Mart Logo"
+        quality={100}
+        priority={true}
+        loading={"eager"}
       />
       <h1>Grocery Mart</h1>
-    </div>
+    </Link>
   );
 }
 export default Logo;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,10 @@ import Logo from "./Logo";
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <Logo />
+    <header className="bg-gray-200">
+      <div className="container py-6">
+        <Logo />
+      </div>
     </header>
   );
 };
