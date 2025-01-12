692ea331c454372837e76bf9117d29857ac3a918
Lenghia0183 <lenghia0183@gmail.com>
2025-01-12 21:13:56 +0700

    Add LabelValue component

---
+++ src/components/LabelValue.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import clsx from "clsx";
+
+type LabelValueProps = {
+  label: React.ReactNode;
+  value: React.ReactNode;
+  labelWidth?: string;
+  labelClassName?: string;
+  valueClassName?: string;
+  className?: string;
+};
+
+const LabelValue: React.FC<LabelValueProps> = ({
+  label,
+  value,
+  labelWidth = "auto",
+  labelClassName = "",
+  valueClassName = "",
+  className = "",
+}) => {
+  return (
+    <div className={clsx("flex items-center text-lg text-dark", className)}>
+      <div
+        className={clsx("font-semibold", labelClassName)}
+        style={{ width: labelWidth }}
+      >
+        {label}
+      </div>
+      <div className={clsx("ml-2", valueClassName)}>{value}</div>
+    </div>
+  );
+};
+
+LabelValue.displayName = "LabelValue";
+
+export default LabelValue;
