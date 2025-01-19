caa010637ba7bbc45663afdc022cd4cb4a6e63b6
Lenghia0183 <lenghia0183@gmail.com>
2025-01-19 23:48:35 +0700

    Update

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 
-// Custom hook để debounce
 function useDebounce(value: string, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
