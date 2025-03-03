8420e629966b10106f8b532472f501acc301bbbf
Lenghia0183 <lenghia0183@gmail.com>
2025-03-03 17:08:59 +0700

    update user provider

--- src/components/AvatarMenu.tsx
+++ src/components/AvatarMenu.tsx
@@ -1,10 +1,12 @@
-import clsx from 'clsx';
 import { useState } from 'react';
+import Link from 'next/link';
 import Icon from './Icon';
 import { useTheme } from '@/context/ThemeProvider';
 import { useLanguage } from '@/context/LanguageProvider';
 import { Locale, Locales } from '@/config/locales';
 import { useTranslations } from 'next-intl';
+import { PATH } from '@/constants/path';
+import { useUser } from '@/context/userProvider';
 
 interface MenuItem {
   label: string;
@@ -13,33 +15,39 @@ interface MenuItem {
   divide?: boolean;
   value?: string;
   action?: () => void;
+  to?: string; // Sử dụng thuộc tính `to` để truyền link
 }
 
 const AvatarMenu = () => {
   const { setTheme } = useTheme();
   const { changeLanguage } = useLanguage();
+  const t = useTranslations('common');
+  const { logoutUser } = useUser();
+
   const handleThemeChange = (newTheme: string) => {
     setTheme(newTheme);
   };
 
-  const t = useTranslations('common');
-
   const handleLanguageChange = (newLanguage: Locale) => {
     changeLanguage(newLanguage);
   };
+
   const menuItems: MenuItem[] = [
     {
       label: t('account'),
       icon: <Icon name="account" color="inherit" strokeWidth={1.75} />,
+      to: PATH.PROFILE_EDIT,
     },
     {
       label: t('cart'),
       icon: <Icon name="cart" color="inherit" strokeWidth={1.8} />,
+      to: PATH.CART,
     },
     {
       label: t('favorites'),
       icon: <Icon name="heart" color="inherit" strokeWidth={1.8} />,
       divide: true,
+      to: PATH.FAVORITE,
     },
     {
       label: t('settings'),
@@ -77,11 +85,14 @@ const AvatarMenu = () => {
     {
       label: t('logout'),
       icon: <Icon name="logout" color="inherit" size={1.3} strokeWidth={15} />,
+      action: () => {
+        logoutUser();
+      },
     },
   ];
 
+  // Sử dụng menuStack để hỗ trợ menu lồng nhau
   const [menuStack, setMenuStack] = useState<MenuItem[]>([{ label: 'main', subMenu: menuItems }]);
-
   const currentMenu = menuStack[menuStack.length - 1];
 
   const goBack = () => {
@@ -91,10 +102,10 @@ const AvatarMenu = () => {
   };
 
   return (
-    <ul className="max-w-[240px] rounded-2xl overflow-hidden text-dark font-semibold text-base pb-">
-      <li className="p-5 flex gap-5 item-center">
+    <ul className="max-w-[240px] rounded-2xl overflow-hidden text-dark font-semibold text-base">
+      <li className="p-5 flex gap-5 items-center">
         <div className="h-[50px] w-[50px] rounded-md bg-dark"></div>
-        <div className="">
+        <div>
           <p>Lenghia0183</p>
           <p className="mt-1 text-sm font-normal text-gray-500 truncate w-[120px]" title="Lenghia0183@gmail.com">
             Lenghia0183@gmail.com
@@ -103,36 +114,54 @@ const AvatarMenu = () => {
       </li>
 
       {menuStack.length > 1 && (
-        <li onClick={goBack} className=" hover:bg-gray-400 hover:text-blue-500 cursor-pointer font-semibold">
-          <div className=" py-2 px-5 flex items-center justify-between">
+        <li onClick={goBack} className="hover:bg-gray-400 hover:text-blue-500 cursor-pointer font-semibold">
+          <div className="py-2 px-5 flex items-center gap-2">
             <Icon name="backArrow" color="inherit" size={1.5} />
             {t('back')}
           </div>
           <div className="h-[1px] my-1 mx-1 bg-gray-500"></div>
         </li>
       )}
-      {currentMenu.subMenu?.map((item, index) => (
-        <li
-          key={index}
-          className="relative hover:text-blue-500"
-          onClick={() => {
-            if (item.action) {
-              item.action();
-            }
-          }}
-        >
-          <div
-            onClick={() => item.subMenu && setMenuStack((prev) => [...prev, item])}
-            className={clsx('py-3 px-5 hover:bg-gray-400 cursor-pointer flex justify-between items-center')}
-          >
-            <div className="flex items-center justify-between text-inherit w-full">
-              <div>{item.label}</div>
-              {item.icon && item.icon}
-            </div>
-          </div>
-          {item.divide && <div className="h-[1px] my-1 mx-5 bg-gray-400"></div>}
-        </li>
-      ))}
+
+      {currentMenu.subMenu?.map((item, index) => {
+        if (item.to) {
+          return (
+            <li key={index} className="relative hover:text-blue-500 cursor-pointer">
+              <Link href={item.to} rel="noopener noreferrer">
+                <div className="py-3 px-5 hover:bg-gray-400 flex justify-between items-center">
+                  <div className="flex items-center justify-between text-inherit w-full">
+                    <div>{item.label}</div>
+                    {item.icon && item.icon}
+                  </div>
+                </div>
+              </Link>
+              {item.divide && <div className="h-[1px] my-1 mx-5 bg-gray-400"></div>}
+            </li>
+          );
+        } else {
+          return (
+            <li
+              key={index}
+              className="relative hover:text-blue-500 cursor-pointer"
+              onClick={() => {
+                if (item.subMenu) {
+                  setMenuStack((prev) => [...prev, item]);
+                } else if (item.action) {
+                  item.action();
+                }
+              }}
+            >
+              <div className="py-3 px-5 hover:bg-gray-400 flex justify-between items-center">
+                <div className="flex items-center justify-between text-inherit w-full">
+                  <div>{item.label}</div>
+                  {item.icon && item.icon}
+                </div>
+              </div>
+              {item.divide && <div className="h-[1px] my-1 mx-5 bg-gray-400"></div>}
+            </li>
+          );
+        }
+      })}
     </ul>
   );
 };
--- src/context/userProvider.tsx
+++ src/context/userProvider.tsx
@@ -1,12 +1,17 @@
 'use client';
 
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { useGetMe } from '@/services/api/https/user';
 import { UserData } from '@/types/user';
 import { LoginResponse } from '@/types/auth';
 import { setLocalStorageItem } from '@/utils';
 import { useGetMyFavorite } from '@/services/api/https/favorite';
 import { useGetMyCart } from '@/services/api/https/cart';
+import { useRouter } from '@/i18n/routing';
+import { PATH } from '@/constants/path';
+import { nextApi } from '@/services/api/axios';
+import { useToast } from './toastProvider';
+import { useTranslations } from 'next-intl';
 
 interface UserContextType {
   userData: UserData | null;
@@ -23,10 +28,10 @@ const UserContext = createContext<UserContextType>({
   userData: null,
   isLoading: true,
   userFavoritesCount: 0,
+  userCartTotalMoney: 0,
   loginUser: () => {},
   logoutUser: () => {},
   refreshUserFavorites: () => {},
-  userCartTotalMoney: 0,
   refreshUserCart: () => {},
 });
 
@@ -35,32 +40,67 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
   const { data: userFavoriteData, mutate: refreshUserFavorites } = useGetMyFavorite({ page: 1, limit: 50 });
   const { data: userCartData, mutate: refreshUserCart } = useGetMyCart();
 
-  console.log('cartData', userCartData);
+  const { success } = useToast();
+  const tCommon = useTranslations('common');
+
+  const router = useRouter();
+
+  const [userData, setUserData] = useState<UserData | null>(data ?? null);
+  const [userFavoritesCount, setUserFavoritesCount] = useState<number>(0);
+  const [userCartTotalMoney, setUserCartTotalMoney] = useState<number>(0);
+
+  useEffect(() => {
+    setUserData(data ?? null);
+  }, [data]);
+
+  useEffect(() => {
+    console.log('test');
+    if (userFavoriteData?.data) {
+      setUserFavoritesCount(userFavoriteData.data.totalResult || 0);
+    }
+  }, [userFavoriteData]);
+
+  useEffect(() => {
+    if (userCartData?.data) {
+      setUserCartTotalMoney(userCartData.data.cartTotalMoney || 0);
+    }
+  }, [userCartData]);
 
   const loginUser = (loginData: LoginResponse | undefined) => {
     setLocalStorageItem('user', loginData?.user);
     setLocalStorageItem('accessToken', loginData?.accessToken);
     setLocalStorageItem('refreshToken', loginData?.refreshToken);
 
+    if (loginData?.user) {
+      setUserData(loginData.user);
+    }
     refreshUserFavorites();
     refreshGetMe();
   };
 
-  const logoutUser = () => {
+  const logoutUser = async () => {
     localStorage.removeItem('user');
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
-  };
 
-  console.log('userData', data);
+    setUserFavoritesCount(0);
+    setUserCartTotalMoney(0);
+    setUserData(null);
+    nextApi.post('/auth/set-cookie', {
+      accessToken: '',
+      refreshToken: '',
+    });
+    success(tCommon('logoutSuccessful'));
+    router.push(PATH.LOGIN);
+  };
 
   return (
     <UserContext.Provider
       value={{
-        userData: data ?? null,
+        userData,
         isLoading: isLoadingGetMe || isValidatingGetMe,
-        userFavoritesCount: userFavoriteData?.data?.totalResult || 0,
-        userCartTotalMoney: userCartData?.data?.cartTotalMoney || 0,
+        userFavoritesCount,
+        userCartTotalMoney,
         refreshUserFavorites,
         refreshUserCart,
         loginUser,
