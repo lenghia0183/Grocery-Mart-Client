bd2f84234754100158cbc3d5af7eac2eb2d71829
Lenghia0183 <lenghia0183@gmail.com>
2025-03-08 11:57:47 +0700

    update revalidate for home page

--- src/app/[locale]/(user)/page.tsx
+++ src/app/[locale]/(user)/page.tsx
@@ -8,7 +8,7 @@ import { getQueryState } from '@/utils/getQueryState';
 import { getTranslations } from 'next-intl/server';
 import { Suspense } from 'react';
 
-export const revalidate = 60 * 15; // 15 phút (900 giây)
+export const revalidate = 900; // 15 phút (900 giây)
 
 export async function generateStaticParams() {
   return [1, 2, 3].map((page) => ({ searchParams: { page: page.toString() } }));
